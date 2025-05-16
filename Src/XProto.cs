94db9436b1f6f86f5a48bb193757d0c27e9bfd7b
Ov poddar Windows <ayanpoddar0@gmail.com>
2025-05-17 00:39:24 +0530

    Add IXProto interface and XProto implementation
    This commit introduces a new interface `IXProto` that defines various methods for window management and graphics operations. The `XProto` class implements this interface, currently throwing `NotImplementedException` for each method, indicating that the functionality is still under development. Additionally, the solution files have been updated to reflect the new project structure, with the creation of `Xcsb.sln` to replace the removed `X11CS.sln`.

---
+++ Src/XProto.cs
@@ -0,0 +1,645 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Src;
+internal class XProto : IXProto
+{
+    private bool _disposedValue;
+
+    void IXProto.AllocColor()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.AllocColorCells()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.AllocColorPlanes()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.AllocNamedColor()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.AllowEvents()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.Bell()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.ChangeActivePointerGrab()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.ChangeGC()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.ChangeHosts()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.ChangeKeyboardControl()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.ChangeKeyboardMapping()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.ChangePointerControl()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.ChangeProperty()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.ChangeSaveSet()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.ChangeWindowAttributes()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.CirculateWindow()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.ClearArea()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.CloseFont()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.ConfigureWindow()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.ConvertSelection()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.CopyArea()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.CopyColormapAndFree()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.CopyGC()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.CopyPlane()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.CreateColormap()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.CreateCursor()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.CreateGC()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.CreateGlyphCursor()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.CreatePixmap()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.CreateWindow()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.DeleteProperty()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.DestroySubwindows()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.DestroyWindow()
+    {
+        throw new NotImplementedException();
+    }
+
+    ValueTask IAsyncDisposable.DisposeAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.FillPoly()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.ForceScreenSaver()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.FreeColormap()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.FreeColors()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.FreeCursor()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.FreeGC()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.FreePixmap()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.GetAtomName()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.GetFontPath()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.GetGeometry()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.GetImage()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.GetInputFocus()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.GetKeyboardControl()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.GetKeyboardMapping()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.GetModifierMapping()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.GetMotionEvents()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.GetPointerControl()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.GetPointerMapping()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.GetProperty()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.GetScreenSaver()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.GetSelectionOwner()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.GetWindowAttributes()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.GrabButton()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.GrabKey()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.GrabKeyboard()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.GrabPointer()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.GrabServer()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.ImageText16()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.ImageText8()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.InstallColormap()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.InternAtom()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.KillClient()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.ListExtensions()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.ListFonts()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.ListFontsWithInfo()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.ListHosts()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.ListInstalledColormaps()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.ListProperties()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.LookupColor()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.MapSubwindows()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.MapWindow()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.NoOperation()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.OpenFont()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.PolyArc()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.PolyFillArc()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.PolyFillRectangle()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.PolyLine()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.PolyPoint()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.PolyRectangle()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.PolySegment()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.PolyText16()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.PolyText8()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.PutImage()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.QueryBestSize()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.QueryColors()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.QueryExtension()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.QueryFont()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.QueryKeymap()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.QueryPointer()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.QueryTextExtents()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.QueryTree()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.RecolorCursor()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.ReparentWindow()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.RotateProperties()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.SendEvent()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.SetAccessControl()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.SetClipRectangles()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.SetCloseDownMode()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.SetDashes()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.SetFontPath()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.SetInputFocus()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.SetModifierMapping()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.SetPointerMapping()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.SetScreenSaver()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.SetSelectionOwner()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.StoreColors()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.StoreNamedColor()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.TranslateCoordinates()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.UngrabButton()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.UngrabKey()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.UngrabKeyboard()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.UngrabPointer()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.UngrabServer()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.UninstallColormap()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.UnmapSubwindows()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.UnmapWindow()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IXProto.WarpPointer()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!_disposedValue)
+        {
+            if (disposing)
+            {
+                // TODO: dispose managed state (managed objects)
+            }
+
+            // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+            // TODO: set large fields to null
+            _disposedValue = true;
+        }
+    }
+
+    // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+    // ~XProto()
+    // {
+    //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+    //     Dispose(disposing: false);
+    // }
+
+    void IDisposable.Dispose()
+    {
+        // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+}
--- Src/Xcsb.cs
+++ Src/Xcsb.cs
@@ -1,6 +1,12 @@
-﻿namespace Src;
+﻿
+namespace Src;
 
 public class Xcsb
 {
 
+    public IXProto Initialized()
+    {
+        var result = new XProto();
+        return result;
+    }
 }
