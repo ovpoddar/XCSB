54daaeea8df760b5b155b7eb795e2ee75e9577b1
Ov poddar Windows <ayanpoddar0@gmail.com>
2025-06-06 22:53:39 +0530

    Add SetInputFocusType struct and update DeletePropertyType
    Introduces a new internal readonly struct `SetInputFocusType` with fields for `Opcode`, `InputFocusMode`, `Length`, `Focus`, and `Time`. Also adds layout and method implementation attributes to both `SetInputFocusType` and the existing `DeletePropertyType` struct, enhancing their structure without removing any existing fields.

--- Src/Models/Requests/Request.cs
+++ Src/Models/Requests/Request.cs
@@ -203,6 +203,18 @@ internal readonly struct DeletePropertyType(uint window, uint atom)
 
 [StructLayout(LayoutKind.Sequential, Pack = 1, Size = 12)]
 [method: MethodImpl(MethodImplOptions.AggressiveInlining)]
+internal readonly struct SetInputFocusType(InputFocusMode mode, uint focus, uint time)
+{
+    public readonly Opcode opcode = Opcode.SetInputFocus;
+    public readonly InputFocusMode Mode = mode;
+    public readonly ushort Length = 3;
+    public readonly uint Focus = focus;
+    public readonly uint Time = time;
+}
+
+
+[StructLayout(LayoutKind.Sequential, Pack = 1, Size = 12)]
+[method: MethodImpl(MethodImplOptions.AggressiveInlining)]
 internal readonly struct ChangeGCType(uint gc, GCMask mask, int argsLength)
 {
     public readonly Opcode opcode = Opcode.ChangeGC;
