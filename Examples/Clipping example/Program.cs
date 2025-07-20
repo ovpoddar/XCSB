9ba5a99194a21e404cf1aa6407d6174719cccfe7
ayan <ayanpoddar0@gmail.com>
2025-07-20 19:52:47 +0530

    Refactor formatting in `Clipping example/Program.cs` for improved readability and consistency.

--- Examples/Clipping example/Program.cs
+++ Examples/Clipping example/Program.cs
@@ -10,20 +10,20 @@ var screen = xcsb.HandshakeSuccessResponseBody.Screens[0];
 var window = xcsb.NewId();
 
 xcsb.CreateWindow(0,
-                  window,
-                  screen.Root,
-                  100, 100, width, height,
-                  2, ClassType.InputOutput,
-                  screen.RootVisualId,
-                  ValueMask.BackgroundPixel | ValueMask.EventMask,
-                  [screen.WhitePixel, (uint)(EventMask.ExposureMask | EventMask.KeyPressMask)]
-                  );
+    window,
+    screen.Root,
+    100, 100, width, height,
+    2, ClassType.InputOutput,
+    screen.RootVisualId,
+    ValueMask.BackgroundPixel | ValueMask.EventMask,
+    [screen.WhitePixel, (uint)(EventMask.ExposureMask | EventMask.KeyPressMask)]
+);
 
 var pixmap = xcsb.NewId();
 xcsb.CreatePixmap(screen.RootDepth!.DepthValue,
-                  pixmap,
-                  screen.Root,
-                  width, height);
+    pixmap,
+    screen.Root,
+    width, height);
 
 var gc = xcsb.NewId();
 xcsb.CreateGC(gc, pixmap, GCMask.Foreground | GCMask.Background, [screen.BlackPixel, screen.WhitePixel]);
@@ -40,24 +40,24 @@ var cursor_gc = xcsb.NewId();
 xcsb.CreateGC(cursor_gc, cursor_pixmap, GCMask.Foreground, [1]);
 
 xcsb.PolySegment(cursor_pixmap, cursor_gc,
-    [
-        new Segment { X1 = 8, Y1 = 0, X2 = 8, Y2 = 15 },
-        new Segment { X1 = 0, Y1 = 8, X2 = 15, Y2 = 8 }
-    ]);
+[
+    new Segment { X1 = 8, Y1 = 0, X2 = 8, Y2 = 15 },
+    new Segment { X1 = 0, Y1 = 8, X2 = 15, Y2 = 8 }
+]);
 xcsb.PolySegment(cursor_mask, cursor_gc,
-    [
-        new Segment { X1 = 8, Y1 = 0, X2 = 8, Y2 = 15 },
-        new Segment { X1 = 0, Y1 = 8, X2 = 15, Y2 = 8 }
-    ]);
+[
+    new Segment { X1 = 8, Y1 = 0, X2 = 8, Y2 = 15 },
+    new Segment { X1 = 0, Y1 = 8, X2 = 15, Y2 = 8 }
+]);
 
 xcsb.CreateCursor(cursor, cursor_pixmap, cursor_mask,
-                 0, 0, 0,
-                 65535, 65535, 65535,
-                 8, 8);
+    0, 0, 0,
+    65535, 65535, 65535,
+    8, 8);
 
 xcsb.RecolorCursor(cursor,
-                  65535, 0, 0,
-                  0, 0, 65535);
+    65535, 0, 0,
+    0, 0, 65535);
 
 xcsb.ChangeWindowAttributes(window, ValueMask.Cursor, [cursor]);
 
@@ -65,18 +65,18 @@ xcsb.ChangeGC(gc, GCMask.Foreground, screen.WhitePixel);
 xcsb.PolyFillRectangle(pixmap, gc, [new Rectangle { X = 0, Y = 0, Width = width, Height = height }]);
 
 xcsb.SetClipRectangles(ClipOrdering.Unsorted,
-                       gc,
-                       0, 0,
-                       [
-                           new Rectangle   { X = 50, Y = 50, Width = 100, Height = 80},
-                           new Rectangle{ X = 200, Y = 100, Width = 120, Height = 60},
-                           new Rectangle{ X = 100, Y = 180, Width = 80, Height = 90}
-                       ]);
+    gc,
+    0, 0,
+    [
+        new Rectangle { X = 50, Y = 50, Width = 100, Height = 80 },
+        new Rectangle { X = 200, Y = 100, Width = 120, Height = 60 },
+        new Rectangle { X = 100, Y = 180, Width = 80, Height = 90 }
+    ]);
 
 xcsb.SetDashes(
-               gc,
-               0,
-               [10, 5, 3, 7]);
+    gc,
+    0,
+    [10, 5, 3, 7]);
 
 xcsb.ChangeGC(gc, GCMask.LineStyle, 1);
 
@@ -89,10 +89,10 @@ xcsb.PolyRectangle(pixmap, gc, [new Rectangle { X = 5, Y = 5, Width = width - 10
 xcsb.ChangeGC(gc, GCMask.ClipMask, 0);
 
 xcsb.PolyRectangle(pixmap, gc, [
-                           new Rectangle   { X = 50, Y = 50, Width = 100, Height = 80},
-                           new Rectangle{ X = 200, Y = 100, Width = 120, Height = 60},
-                           new Rectangle{ X = 100, Y = 180, Width = 80, Height = 90}
-                       ]);
+    new Rectangle { X = 50, Y = 50, Width = 100, Height = 80 },
+    new Rectangle { X = 200, Y = 100, Width = 120, Height = 60 },
+    new Rectangle { X = 100, Y = 180, Width = 80, Height = 90 }
+]);
 
 xcsb.MapWindow(window);
 
@@ -103,6 +103,20 @@ xcsb.FreeGC(cursor_gc);
 
 var isRunning = true;
 
+var windowDetails = xcsb.GetWindowAttributes(window);
+if (windowDetails.Class != ClassResponseType.InputOutput)
+    return;
+var windowGeometry = xcsb.GetGeometry(window);
+if (windowGeometry is { X: 100, Y: 100 })
+    xcsb.ConfigureWindowChecked(window,
+        ConfigureValueMask.X | ConfigureValueMask.Y,
+        [500, 500]);
+
+var query = xcsb.QueryTree(window);
+if (query.Parent != xcsb.HandshakeSuccessResponseBody.Screens[0].Root)
+    return;
+
+
 while (isRunning)
 {
     var evnt = xcsb.GetEvent();
@@ -110,23 +124,20 @@ while (isRunning)
     switch (evnt.Value.EventType)
     {
         case Xcsb.Models.Event.EventType.Expose:
-            {
-
-                xcsb.CopyArea(pixmap,
-                             window,
-                             gc,
-                             0, 0,
-                             0, 0,
-                             width, height);
-                break;
-            }
+        {
+            xcsb.CopyArea(pixmap,
+                window,
+                gc,
+                0, 0,
+                0, 0,
+                width, height);
+            break;
+        }
 
         case Xcsb.Models.Event.EventType.KeyPress or Xcsb.Models.Event.EventType.Error:
             isRunning = false;
             break;
-
     }
-
 }
 
 xcsb.FreePixmap(pixmap);
