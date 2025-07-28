16d0b881374817e0a6ca39195e3dde2e6b4b30a7
ayan <ayanpoddar0@gmail.com>
2025-07-28 23:36:40 +0530

    Add new example "SmokeRunOnResponseProto" to solution demonstrating event handling and client interactions.

---
+++ Examples/SmokeRunOnResponseProto/Program.cs
@@ -0,0 +1,67 @@
+﻿using Xcsb;
+using Xcsb.Masks;
+using Xcsb.Models;
+using Xcsb.Models.Event;
+
+var client = XcsbClient.Initialized();
+var window = client.NewId();
+client.CreateWindowChecked(
+    0,
+    window,
+    client.HandshakeSuccessResponseBody.Screens[0].Root,
+    0, 0, 600, 800, 0, ClassType.InputOutput,
+    client.HandshakeSuccessResponseBody.Screens[0].RootVisualId,
+    ValueMask.BackgroundPixel | ValueMask.EventMask,
+    [0x00ff00, (uint)(EventMask.ExposureMask)]
+);
+client.MapWindowChecked(window);
+
+
+
+
+
+while (true)
+{
+    var Event = client.GetEvent();
+    if (!Event.HasValue) 
+        break;
+    if (Event.Value.EventType == EventType.Error)
+    {
+        Console.WriteLine(Event.Value.ErrorEvent.ErrorCode);
+        // break;
+    }
+
+    if (Event.Value.EventType == EventType.Expose)
+    {
+        var resultGetMotionEvents = client.GetMotionEvents(window, 0, 10000);
+        Console.WriteLine(resultGetMotionEvents.Events.Length);
+        
+        var resultQueryKeymap = client.QueryKeymap();
+        Console.WriteLine("QueryKeymap");
+        var resultGetScreenSaver = client.GetScreenSaver();
+        Console.WriteLine("GetScreenSaver");
+        var resultGetPointerControl = client.GetPointerControl();
+        Console.WriteLine("GetPointerControl");
+        var resultGetModifierMapping = client.GetModifierMapping();
+        Console.WriteLine("GetModifierMapping");
+        var resultGetKeyboardControl = client.GetKeyboardControl();
+        Console.WriteLine("GetKeyboardControl");
+        var resultGetPointerMapping = client.GetPointerMapping();
+        Console.WriteLine("GetPointerMapping");
+        // var resultGetFontPath = client.GetFontPath();
+        
+        
+        // var resultQueryTextExtents = client.QueryTextExtents();
+        // var resultAllocNamedColor = client.AllocNamedColor();
+        // var resultAllocColorCells = client.AllocColorCells();
+        // var resultSetModifierMapping = client.SetModifierMapping();
+        // var resultLookupColor = client.LookupColor();
+        // var resultQueryFont = client.QueryFont();
+        // var resultQueryExtension = client.QueryExtension();
+        // var resultListInstalledColormaps = client.ListInstalledColormaps();
+        // var resultAllocColorPlanes = client.AllocColorPlanes();
+        // var resultQueryBestSize = client.QueryBestSize();
+        // var resultSetPointerMapping = client.SetPointerMapping();
+        
+    }
+}
