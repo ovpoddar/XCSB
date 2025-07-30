a58a1f4a8a5a11a5078a4c0b1148d14c23a46d15
Ov poddar Windows <ayanpoddar0@gmail.com>
2025-07-31 01:45:17 +0530

    Update `GetSelectionOwnerReply` struct with `StructLayout` attribute for memory alignment and size control

--- Src/Models/Response/GetSelectionOwnerReply.cs
+++ Src/Models/Response/GetSelectionOwnerReply.cs
@@ -1,5 +1,8 @@
-﻿namespace Xcsb.Models.Response;
+﻿using System.Runtime.InteropServices;
 
+namespace Xcsb.Models.Response;
+
+[StructLayout(LayoutKind.Sequential, Pack = 1, Size = 32)]
 public readonly struct GetSelectionOwnerReply : IXBaseResponse
 {
     public readonly byte ResponseType; // 1
