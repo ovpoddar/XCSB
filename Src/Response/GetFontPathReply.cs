94d9ef21f0b86c8381a80ce42b588884decda760
ayan <ayanpoddar0@gmail.com>
2025-08-04 23:23:23 +0530

    Add "SmokeTest2" example project, update solution and refactor `GetFontPathReply` to use `Paths` array instead of single string.

--- Src/Response/GetFontPathReply.cs
+++ Src/Response/GetFontPathReply.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Sockets;
+﻿using System.Diagnostics;
+using System.Net.Sockets;
 using System.Text;
 using Xcsb.Helpers;
 using Xcsb.Response.Internals;
@@ -9,19 +10,23 @@ public struct GetFontPathReply
 {
     public byte Reply;
     public ushort Sequence;
-    public string Path;
+    public string[] Paths;
 
     internal GetFontPathReply(GetFontPathResponse response, Socket socket)
     {
         Reply = response.ResponseHeader.Reply;
         Sequence = response.ResponseHeader.Sequence;
         if (response.StringLength == 0)
-            Path = string.Empty;
+            Paths = [];
         else
         {
-            using var buffer = new ArrayPoolUsing<byte>((int)response.ResponseHeader.Length);
-            socket.ReceiveExact(buffer[0..(int)response.ResponseHeader.Length]);
-            Path = Encoding.UTF8.GetString(buffer, 0, response.StringLength);
+            var requiredSize = (int)response.ResponseHeader.Length * 4;
+            using var buffer = new ArrayPoolUsing<byte>(requiredSize);
+            socket.ReceiveExact(buffer[0..requiredSize]);
+            this.Paths = new string[response.StringLength];
+            int i = 0;
+            foreach (var range in GenericHelper.GetNextStrValue(buffer))
+                Paths[i++] = Encoding.UTF8.GetString(buffer, range.Position, range.Length);
         }
     }
 }
