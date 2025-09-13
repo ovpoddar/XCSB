cf0bc359dbe0ef56fb7afbbab2c5b1d1fb2b200e
Ov poddar Windows <ayanpoddar0@gmail.com>
2025-09-13 13:44:40 +0530

    Reorder `Button` enum values for logical consistency and cleanup

--- Src/Models/Button.cs
+++ Src/Models/Button.cs
@@ -4,8 +4,8 @@ public enum Button : byte
 {
     Any = 0,
     LeftButton = 1,
-    RightButton = 2,
-    MiddleButton = 3,
-    ScrollWellUp = 4, //todo :verify
-    ScrollWellDown = 5 //todo :verify
+    MiddleButton = 2,
+    RightButton = 3,
+    ScrollWellUp = 4,
+    ScrollWellDown = 5
 }
