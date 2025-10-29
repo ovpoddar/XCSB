f6cf2995983962abd78793d725c661b5af79957e
Ov poddar Windows <ayanpoddar0@gmail.com>
2025-10-29 23:41:59 +0530

    Update `VoidMethodsTest` to use fixed `windowID` in `CreateWindow` tests and enhance assertion coverage

--- Test/MethodRequestBuilder/VoidMethodsTest.cs
+++ Test/MethodRequestBuilder/VoidMethodsTest.cs
@@ -14,7 +14,7 @@ public class VoidMethodsTest : IDisposable
     }
 
     [Theory]
-    [InlineData(0, 100, 100, 400, 300, 2, ClassType.InputOutput, new byte[] { 1, 2, })]
+    [InlineData(0, 100, 100, 400, 300, 2, ClassType.InputOutput, new byte[] { 1, 0, 10, 0, 0, 0, 96, 0, 56, 4, 0, 0, 100, 0, 100, 0, 144, 1, 44, 1, 2, 0, 1, 0, 35, 0, 0, 0, 2, 8, 0, 0, 1, 128, 0, 0, 255, 255, 255, 0 })]
     public void Create_Window_Test(byte depth, short x, short y, ushort width, ushort height, ushort borderWidth,
         ClassType classType, byte[] result)
     {
@@ -23,9 +23,11 @@ public class VoidMethodsTest : IDisposable
             .GetField("_buffer", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
         var screen = _xProto.HandshakeSuccessResponseBody.Screens[0];
         var bufferClient = (XBufferProto)_xProto.BufferClient;
-
+        uint windowID = 6291456;
+        var c = _xProto.NewId();
+        Assert.Equal(windowID, c);
         // act
-        bufferClient.CreateWindow(depth, _xProto.NewId(), screen.Root, x, y,
+        bufferClient.CreateWindow(depth, windowID, screen.Root, x, y,
             width, height, borderWidth, classType, screen.RootVisualId,
             ValueMask.BackgroundPixel | ValueMask.EventMask,
             [
