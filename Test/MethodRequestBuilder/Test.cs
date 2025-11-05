a4e77004c5683ed7945a085232222b86a2c1f811
Ov poddar Windows <ayanpoddar0@gmail.com>
2025-11-06 01:46:04 +0530

    Add `MethodRequestBuilder` test project with Docker-based test environment
    - Introduced `Test.cs` with an initial placeholder test using the `[Fact]` attribute.
    - Added `testenv.Dockerfile` for a .NET SDK-based test environment with debugging tools.
    - Configured `testEnvironments.json` for Dockerized testing with a predefined `linux dotnet core` environment.

---
+++ Test/MethodRequestBuilder/Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodRequestBuilder.Test;
+
+public class Test
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(true);
+    }
+}
