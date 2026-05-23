c8295c11bcef54b21444adb7dd34984d4505df20
ayan <ayanpoddar9@gmail.com>
2026-05-23 18:30:49 +0530

    Expand `XInputProto` and `IVoidProto` to include additional device-related methods and update `OpCode` enums for consistent naming conventions.

--- Src/Extensions/Xcsb.Extension.XInput/Implementation/XInputProto.cs
+++ Src/Extensions/Xcsb.Extension.XInput/Implementation/XInputProto.cs
@@ -8,6 +8,7 @@ using Xcsb.Connection.Response.Replies;
 using Xcsb.Extension.XInput.Infrastructure;
 using Xcsb.Extension.XInput.Requests;
 using Xcsb.Extension.XInput.Response.Replies;
+using Xcsb.Models;
 
 namespace Xcsb.Extension.XInput.Implementation;
 
@@ -60,6 +61,16 @@ internal sealed class XInputProto : IXinputRequest
         return new ResponseProto(_extensionInternal.Transport.SocketOut.Sequence);
     }
 
+    public ResponseProto SelectExtensionEvent(uint window, ushort numClasses)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ResponseProto ChangeDeviceDontPropagateList(uint window, ushort numClasses, byte mode)
+    {
+        throw new NotImplementedException();
+    }
+
     public ResponseProto UngrabDevice(uint time, byte deviceId)
     {
         var request = new UngrabDeviceType(this._response.MajorOpcode, time, deviceId);
@@ -67,4 +78,132 @@ internal sealed class XInputProto : IXinputRequest
         return new ResponseProto(_extensionInternal.Transport.SocketOut.Sequence);
     }
 
+    public ResponseProto GrabDeviceKey(uint grabWindow, ushort numClasses, ushort modifiers, byte modifierDevice,
+        byte grabbedDevice, byte key, byte thisDeviceMode, byte otherDeviceMode, byte ownerEvents)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ResponseProto UngrabDeviceKey(uint grabWindow, ushort modifiers, byte modifierDevice, byte key, byte grabbedDevice)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ResponseProto GrabDeviceButton(uint grabWindow, byte grabbedDevice, byte modifierDevice, ushort numClasses,
+        ushort modifiers, byte thisDeviceMode, byte otherDeviceMode, byte button, byte ownerEvents)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ResponseProto UngrabDeviceButton(uint grabWindow, ushort modifiers, byte modifierDevice, byte button,
+        byte grabbedDevice)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ResponseProto AllowDeviceEvents(uint time, byte mode, byte deviceId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ResponseProto SetDeviceFocus(uint focus, uint time, byte revertTo, byte deviceId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ResponseProto ChangeFeedbackControl(uint mask, byte deviceId, byte feedbackId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ResponseProto ChangeDeviceKeyMapping(byte deviceId, byte firstKeycode, byte keysymsPerKeycode, byte keycodeCount)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ResponseProto DeviceBell(byte deviceId, byte feedbackId, byte feedbackClass, sbyte percent)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ResponseProto ChangeDeviceProperty(ATOM property, ATOM type, byte deviceId, byte format, byte mode, uint numItems)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ResponseProto DeleteDeviceProperty(ATOM property, byte deviceId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ResponseProto XiWarpPointer(uint srcWin, uint dstWin, int srcX, int srcY, ushort srcWidth, ushort srcHeight, int dstX,
+        int dstY, ushort deviceId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ResponseProto XiChangeCursor(uint window, uint cursor, ushort deviceId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ResponseProto XiChangeHierarchy(byte numChanges)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ResponseProto XiSetClientPointer(uint window, ushort deviceId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ResponseProto XiSelectEvents(uint window, ushort numMask)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ResponseProto XiSetFocus(uint window, uint time, ushort deviceId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ResponseProto XiUngrabDevice(uint time, ushort deviceId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ResponseProto XiAllowEvents(uint time, ushort deviceId, byte eventMode, uint touchid, uint grabWindow)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ResponseProto XiPassiveUngrabDevice(uint grabWindow, uint detail, ushort deviceId, ushort numModifiers, byte grabType)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ResponseProto XiChangeProperty(ushort deviceId, byte mode, byte format, ATOM property, ATOM type, uint numItems)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ResponseProto XiDeleteProperty(ushort deviceId, ATOM property)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ResponseProto XiBarrierReleasePointer(uint numBarriers)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ResponseProto SendExtensionEvent(uint destination, byte deviceId, byte propagate, ushort numClasses, byte numEvents)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ChangeDeviceControlReply ChangeDeviceControl(ushort controlId, byte deviceId)
+    {
+        throw new NotImplementedException();
+    }
 }
--- Src/Extensions/Xcsb.Extension.XInput/Infrastructure/VoidProto/IVoidProto.cs
+++ Src/Extensions/Xcsb.Extension.XInput/Infrastructure/VoidProto/IVoidProto.cs
@@ -1,8 +1,52 @@
 using Xcsb.Connection.Response;
+using Xcsb.Models;
 
 namespace Xcsb.Extension.XInput.Infrastructure.VoidProto;
 
 public interface IVoidProto
 {
-	ResponseProto AllowDeviceEvents(uint time, byte mode, byte deviceId);
+    ResponseProto CloseDevice(byte deviceId);
+    ResponseProto SelectExtensionEvent(uint window, ushort numClasses);
+    ResponseProto ChangeDeviceDontPropagateList(uint window, ushort numClasses, byte mode);
+    ResponseProto UngrabDevice(uint time, byte deviceId);
+
+    ResponseProto GrabDeviceKey(uint grabWindow, ushort numClasses, ushort modifiers, byte modifierDevice,
+        byte grabbedDevice, byte key, byte thisDeviceMode, byte otherDeviceMode, byte ownerEvents);
+
+    ResponseProto UngrabDeviceKey(uint grabWindow, ushort modifiers, byte modifierDevice, byte key, byte grabbedDevice);
+
+    ResponseProto GrabDeviceButton(uint grabWindow, byte grabbedDevice, byte modifierDevice, ushort numClasses,
+        ushort modifiers, byte thisDeviceMode, byte otherDeviceMode, byte button, byte ownerEvents);
+
+    ResponseProto UngrabDeviceButton(uint grabWindow, ushort modifiers, byte modifierDevice, byte button,
+        byte grabbedDevice);
+
+    ResponseProto AllowDeviceEvents(uint time, byte mode, byte deviceId);
+    ResponseProto SetDeviceFocus(uint focus, uint time, byte revertTo, byte deviceId);
+    ResponseProto ChangeFeedbackControl(uint mask, byte deviceId, byte feedbackId);
+    ResponseProto ChangeDeviceKeyMapping(byte deviceId, byte firstKeycode, byte keysymsPerKeycode, byte keycodeCount);
+    ResponseProto DeviceBell(byte deviceId, byte feedbackId, byte feedbackClass, sbyte percent);
+    ResponseProto ChangeDeviceProperty(ATOM property, ATOM type, byte deviceId, byte format, byte mode, uint numItems);
+    ResponseProto DeleteDeviceProperty(ATOM property, byte deviceId);
+
+    ResponseProto XiWarpPointer(uint srcWin, uint dstWin, int srcX, int srcY, ushort srcWidth, ushort srcHeight,
+        int dstX, int dstY, ushort deviceId);
+
+    ResponseProto XiChangeCursor(uint window, uint cursor, ushort deviceId);
+    ResponseProto XiChangeHierarchy(byte numChanges);
+    ResponseProto XiSetClientPointer(uint window, ushort deviceId);
+    ResponseProto XiSelectEvents(uint window, ushort numMask);
+    ResponseProto XiSetFocus(uint window, uint time, ushort deviceId);
+    ResponseProto XiUngrabDevice(uint time, ushort deviceId);
+    ResponseProto XiAllowEvents(uint time, ushort deviceId, byte eventMode, uint touchid, uint grabWindow);
+
+    ResponseProto XiPassiveUngrabDevice(uint grabWindow, uint detail, ushort deviceId, ushort numModifiers,
+        byte grabType);
+
+    ResponseProto XiChangeProperty(ushort deviceId, byte mode, byte format, ATOM property, ATOM type, uint numItems);
+    ResponseProto XiDeleteProperty(ushort deviceId, ATOM property);
+    ResponseProto XiBarrierReleasePointer(uint numBarriers);
+
+    ResponseProto SendExtensionEvent(uint destination, byte deviceId, byte propagate, ushort numClasses,
+        byte numEvents);
 }
