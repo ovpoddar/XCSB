41d4aaee60030b3396735ff53b1be32bf81a66c8
ayan <ayanpoddar9@gmail.com>
2026-07-04 23:39:45 +0530

    Add `GrabMode`, `GrabMode22`, and `GrabOwner` enums; update `IResponceProto` methods with new types for improved clarity and structure.

--- Src/Extensions/Xcsb.Extension.XInput/Infrastructure/ResponceProto/IResponceProto.cs
+++ Src/Extensions/Xcsb.Extension.XInput/Infrastructure/ResponceProto/IResponceProto.cs
@@ -16,32 +16,40 @@ public interface IResponceProto
     GetDeviceMotionEventsReply GetDeviceMotionEvents(uint start, uint stop, byte deviceId);
     ChangeKeyboardDeviceReply ChangeKeyboardDevice(byte deviceId);
     ChangePointerDeviceReply ChangePointerDevice(byte xAxis, byte yAxis, byte deviceId);
-    GrabDeviceReply GrabDevice(uint grabWindow, uint time, ushort numClasses, byte thisDeviceMode, byte otherDeviceMode,
-        byte ownerEvents, byte deviceId);
+
+    GrabDeviceReply GrabDevice(uint grabWindow, uint time, GrabMode thisDeviceMode, GrabMode otherDeviceMode,
+        bool ownerEvents, byte deviceId, uint[] classes);
+
     GetDeviceFocusReply GetDeviceFocus(byte deviceId);
     GetFeedbackControlReply GetFeedbackControl(byte deviceId);
     GetDeviceKeyMappingReply GetDeviceKeyMapping(byte deviceId, byte firstKeycode, byte count);
     GetDeviceModifierMappingReply GetDeviceModifierMapping(byte deviceId);
     SetDeviceModifierMappingReply SetDeviceModifierMapping(byte deviceId, byte keycodesPerModifier);
     GetDeviceButtonMappingReply GetDeviceButtonMapping(byte deviceId);
-    SetDeviceButtonMappingReply SetDeviceButtonMapping(byte deviceId, byte mapSize);
+    SetDeviceButtonMappingReply SetDeviceButtonMapping(byte deviceId, byte[] map);
     QueryDeviceStateReply QueryDeviceState(byte deviceId);
-    SetDeviceValuatorsReply SetDeviceValuators(byte deviceId, byte firstValuator, byte numValuators);
+    SetDeviceValuatorsReply SetDeviceValuators(byte deviceId, byte firstValuator, int[] valuators);
     GetDeviceControlReply GetDeviceControl(ushort controlId, byte deviceId);
     ListDevicePropertiesReply ListDeviceProperties(byte deviceId);
+
     GetDevicePropertyReply GetDeviceProperty(ATOM property, ATOM type, uint offset, uint len, byte deviceId,
         byte delete);
+
     XiQueryPointerReply XiQueryPointer(uint window, InputDevice deviceId);
     XiGetClientPointerReply XiGetClientPointer(uint window);
     XiQueryVersionReply XiQueryVersion(ushort majorVersion, ushort minorVersion);
     XiQueryDeviceReply XiQueryDevice(InputDevice deviceId);
     XiGetFocusReply XiGetFocus(InputDevice deviceId);
-    XiGrabDeviceReply XiGrabDevice(uint window, uint time, uint cursor, InputDevice deviceId, byte mode,
-        byte pairedDeviceMode, byte ownerEvents, ushort maskLen);
-    XiPassiveGrabDeviceReply XiPassiveGrabDevice(uint time, uint grabWindow, uint cursor, uint detail, InputDevice deviceId,
-        ushort numModifiers, ushort maskLen, byte grabType, byte grabMode, byte pairedDeviceMode, byte ownerEvents);
+    XiGrabDeviceReply XiGrabDevice(uint window, uint time, uint cursor, InputDevice deviceId, GrabMode mode,
+        GrabMode pairedDeviceMode, GrabOwner ownerEvents, uint[] classes);
+    XiPassiveGrabDeviceReply XiPassiveGrabDevice(uint time, uint grabWindow, uint cursor, uint detail,
+        InputDevice deviceId, GrabType grabType, GrabMode22 grabMode, GrabMode pairedDeviceMode, GrabOwner ownerEvents, 
+        uint[] mask, uint[] modifiers);
+
     XiListPropertiesReply XiListProperties(InputDevice deviceId);
+
     XiGetPropertyReply XiGetProperty(InputDevice deviceId, byte delete, ATOM property, ATOM type, uint offset,
         uint len);
+
     XiGetSelectedEventsReply XiGetSelectedEvents(uint window);
 }
---
+++ Src/Extensions/Xcsb.Extension.XInput/Models/GrabMode.cs
@@ -0,0 +1,7 @@
+namespace Xcsb.Extension.XInput.Models;
+
+public enum GrabMode : byte
+{
+    Synchronous = 0,
+    Asynchronous = 1,
+}
